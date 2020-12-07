2d3e9946161d01d29dbf640569a3f5cdf79779e5
123lwc <2374543351@qq.com>
2020-12-07 19:25:38 +0800

    123456

---
+++ src/com/test/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello World");
+        }
+        List<User> users = new ArrayList<User>();
+        for (User user : users) {
+
+        }
+        String username = "123";
+
+    }
+}
+
+
+
